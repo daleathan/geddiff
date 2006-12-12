fe55e9242492daf667fc170fc62b74bd3a8f08e5
oyevato <oyevato@68adafe7-e61b-0410-87be-779de8dc511d>
2006-12-12 04:07:46 +0000

    commit some changes

--- src/geddiff.cpp
+++ src/geddiff.cpp
@@ -8,6 +8,8 @@ using namespace std;
 #include <gom.h>
 #include "geddiff-prog.h"
 
+#include "gedcom_pp.h"
+
 void my_message_handler (Gedcom_msg_type type, char *msg)
 {
 	cout << msg << endl;
@@ -16,11 +18,12 @@ void my_message_handler (Gedcom_msg_type type, char *msg)
 
 int main(int argc, char* argv[])
 {
-	gedcom_init();
-	gedcom_set_message_handler(my_message_handler);
-	int result = gom_parse_file("myfamily.ged");
-	individual* file1_person = gom_get_first_individual();
+	//gedcom_init();
+	//gedcom_set_message_handler(my_message_handler);
+	//int result = gom_parse_file("dbg/file1.ged");
+	//individual* file1_person = gom_get_first_individual();
 //	GeddiffProg prog;
 //	prog.run(argc, argv);
+	Gedcom file;
 	
 }
